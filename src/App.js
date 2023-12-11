76ddf1a53bcd3d422df6b0e8d3b6b6a664293b0c
cotasi <cotasi135@gmail.com>
2023-12-11 12:42:36 +0900

    리액트1

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <img src="/logo512.png" alt="logo" />
+         <p>소스 호출하기 (public은 루트 디렉토리 /로 하면 됩니다.)</p>
+         <p>src 안의 넣은 소스는 node의 컴파일 과정을 거치므로 </p>
       </header>
     </div>
   );
---
+++ src/layout/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+            <p>내가 만든 컴포넌트</p>
+            <p>반드시 대문자, 함수 내의 return 식이 있어야 한다.</p>
+            <p>반드시 export 문이 있어야 한다.</p>
+        </>
+    );
+}
+
+export default Header;
